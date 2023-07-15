05612d46bae280f6506a8b4b21b26cb3c73b7aa6
Humed-Muhammad <abc45171@gmail.com>
2023-07-15 23:44:08 +0300

    👷 Fetch: Stocks fetcher service

--- src/app/screens/Markets/service/index.ts
+++ src/app/screens/Markets/service/index.ts
@@ -1,10 +1,23 @@
 import { pb } from 'src/utils/pocketbase';
 import useSWR from 'swr';
-import { ICategoryTypes } from '../types';
+import { ICategoryTypes, IStocksType } from '../types';
 
 export const useMarketList = () => {
   const fetcher = () => pb.collection('category').getFullList<ICategoryTypes>();
   const { data, error, isLoading, isValidating } = useSWR(`category`, fetcher);
+
+  return { data, error, isLoading, isValidating };
+};
+
+export const useStockFetcher = (categoryId: string, fetch: boolean) => {
+  const fetcher = () =>
+    pb.collection('stocks').getList<IStocksType>(1, 50, {
+      filter: `categoryId ~ "${categoryId}"`,
+    });
+  const { data, error, isLoading, isValidating } = useSWR(
+    !fetch ? null : [`stocks`, categoryId],
+    fetcher,
+  );
 
   return { data, error, isLoading, isValidating };
 };
