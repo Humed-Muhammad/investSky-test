72aefb152a1b5fd6122e0f7b352572c58d252418
Humed-Muhammad <abc45171@gmail.com>
2023-07-15 13:58:39 +0300

    ✨ New: Creating containers core components and re-using Box for both Flex and Container

--- src/app/components/Core/Flex.tsx
+++ src/app/components/Core/Flex.tsx
@@ -2,8 +2,6 @@ import styled from 'styled-components/native';
 import { BoxProps } from './types';
 import { Box } from './Box';
 
-export const Container = styled(Box)<BoxProps>`
+export const Flex = styled(Box)<BoxProps>`
   display: flex;
-  justify-content: center;
-  align-items: center;
 `;
