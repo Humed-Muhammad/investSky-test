4524557ab06a8e1c7f455bc1f3a7ddb565593c56
Humed-Muhammad <abc45171@gmail.com>
2023-07-16 13:56:01 +0300

    ✨ NEW: Common Types for typescript files

---
+++ src/utils/types/types.ts
@@ -0,0 +1,44 @@
+/**
+ * Learn more about using TypeScript with React Navigation:
+ * https://reactnavigation.org/docs/typescript/
+ */
+
+import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
+import {
+  CompositeScreenProps,
+  NavigatorScreenParams,
+} from '@react-navigation/native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
+
+export type RootStackParamList = {
+  Root: NavigatorScreenParams<RootTabParamList> | undefined;
+  Login: undefined;
+  Modal: undefined;
+  NotFound: undefined;
+};
+
+export type RootStackScreenProps<Screen extends keyof RootStackParamList> =
+  NativeStackScreenProps<RootStackParamList, Screen>;
+
+export type RootTabParamList = {
+  Markets: undefined;
+  TabTwo: undefined;
+  Login?: undefined;
+
+  Portfolio?: undefined;
+  // [INSERT NEW SCREEN KEY ABOVE] < Needed for generating screen
+};
+
+export type RootTabScreenProps<Screen extends keyof RootTabParamList> =
+  CompositeScreenProps<
+    BottomTabScreenProps<RootTabParamList, Screen>,
+    NativeStackScreenProps<RootStackParamList>
+  >;
+
+export type DateType = '1D' | '7D' | '1M' | '3M' | '1Y';
