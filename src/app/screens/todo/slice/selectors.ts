8dd08e329fcf62c51daab5c10fb5bca52f235b15
Humed-Muhammad <abc45171@gmail.com>
2023-07-16 18:00:58 +0300

    ⚰️ DEAD: removing dead code

--- src/app/screens/todo/slice/selectors.ts
+++
@@ -1,8 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-import { RootState } from 'src/types';
-import { initialState } from '.';
-
-export const selectFarmer = createSelector(
-  [(state: RootState) => state?.farmerDetails],
-  state => state.isGettingFarmer,
-);
--- src/app/screens/todo/slice/types.ts
+++
@@ -1,4 +0,0 @@
-/* --- STATE --- */
-export interface IFarmerDetails {
-  isGettingFarmer?: boolean;
-}
